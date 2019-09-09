3a68e194c68e8497152cfd9bc469215528fef4fb
Artiom Neganov <aneganov@gmail.com>
2019-09-09 15:15:27 +0300

    Reinitialized repository as a clone of composer create-project (2)

--- scripts/adb-push.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-adb push ./export/DataMincer.Starter.apkg  /storage/emulated/0/AnkiDroid/
--- scripts/watchreload.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-find bundles/01-anki-colors | entr -s 'composer dm unit generate -- --filter=01-anki-colors; ./scripts/reload-browser.sh Chrome'
